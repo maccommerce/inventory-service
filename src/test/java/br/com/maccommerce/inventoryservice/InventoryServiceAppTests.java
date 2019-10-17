b8c0402a2968e8e74f5ee9bb226110933a6d0fa6
Wellington Costa <wellington.costa128@gmail.com>
2019-10-17 16:46:03 -0300

    Initial project structure.

---
+++ src/test/java/br/com/maccommerce/inventoryservice/InventoryServiceAppTests.java
@@ -0,0 +1,13 @@
+package br.com.maccommerce.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
